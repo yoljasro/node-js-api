56a68a20b469a342156bcf64e67a0b89a10a54fc
Jasur Saidaliyev <saidaliyevjasur450@gmail.com>
2022-12-16 10:57:59 +0500

    node js first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+const importData = require("./data.json");
+const cardData = require("./card-data.json");
+const swaggerJSDOC = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+let port = process.env.PORT || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node Js API from JasurBek",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./index.js"],
+};
+const swaggerSpec = swaggerJSDOC(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /getData:
+ *   get:
+ *     description: This is a get api call
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+/**
+ * @swagger
+ * /saveData:
+ *   post:
+ *     description: This is post method
+ *     responses:
+ *       201:
+ *         description: Success or Saved
+ * 403 :
+ *      description : Unauthorized
+ *parameters : 
+    -name : TITLE
+    in : formData
+    required : true
+ */
+
+    /**
+ * @swagger
+ * /updateData:
+ *   put:
+ *     description: This is update method
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+      /**
+ * @swagger
+ * /deleteData:
+ *   delete:
+ *     description: This is delete method api
+ *     responses:
+ *       200:
+ *         description: deleted succes
+ */
+
+app.get("/", (req, res) => {
+  res.send("Hello  world");
+});
+
+app.get("/getData", (req, res) => {
+  res.send(importData);
+});
+
+app.post("/saveData", (req, res) => {
+  res.status(201).send("saved succesfuly");
+});
+
+app.put("/updateData", (req, res) => {
+  res.send("updated succesfully");
+});
+
+app.delete("/deleteData" , (req, res) => {
+  res.send("deleted succesfully")
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port https://localhost:${port}`);
+});
